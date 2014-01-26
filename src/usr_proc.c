6657fddddf62dc5a6ca113507e9f2cdf6ef1a02c
eva9202403 <yy2ho@uwaterloo.ca>
2014-01-25 19:02:43 -0500

    Update usr_proc.c

--- src/usr_proc.c
+++ src/usr_proc.c
@@ -1,50 +1,141 @@
-/* @brief: rtx.h User API prototype, this is only an example
+/**
+ * @file:   usr_proc.c
+ * @brief:  Two user processes: proc1 and proc2
  * @author: Yiqing Huang
- * @date: 2014/01/17
+ * @date:   2014/01/17
+ * NOTE: Each process is in an infinite loop. Processes never terminate.
  */
-#ifndef RTX_H_
-#define RTX_H_
 
-/* ----- Definitations ----- */
-#define RTX_ERR -1
-#define NULL 0
-#define NUM_TEST_PROCS 6
-/* Process Priority. The bigger the number is, the lower the priority is*/
-#define HIGH    0
-#define MEDIUM  1
-#define LOW     2
-#define LOWEST  3
+#include "rtx.h"
+#include "uart_polling.h"
+#include "usr_proc.h"
 
-/* ----- Types ----- */
-typedef unsigned int U32;
+#ifdef DEBUG_0
+#include "printf.h"
+#endif /* DEBUG_0 */
+
+/* --Definition-- */
+#define ARRAY_SIZE 10
 
 /* initialization table item */
-typedef struct proc_init
-{	
-	int m_pid;	        /* process id */ 
-	int m_priority;         /* initial priority, not used in this example. */ 
-	int m_stack_size;       /* size of stack in words */
-	void (*mpf_start_pc) ();/* entry point of the process */    
-} PROC_INIT;
+PROC_INIT g_test_procs[NUM_TEST_PROCS];
+/* initialization null process*/
+PROC_INIT null_proc;
 
-/* ----- RTX User API ----- */
-#define __SVC_0  __svc_indirect(0)
+void set_test_procs() {
+	int i;
+	for( i = 0; i < NUM_TEST_PROCS; i++ ) {
+		g_test_procs[i].m_pid = (U32)(i+1);
+		g_test_procs[i].m_stack_size = 0x100;
+	}
+	
+	null_proc.m_pid = (U32) 0;
+	null_proc.m_priority = LOWEST;
+	null_proc.m_stack_size = 0x100;
+	
+	g_test_procs[0].m_priority = HIGH;
+	g_test_procs[1].m_priority = MEDIUM;
+	g_test_procs[2].m_priority = LOW;
+	g_test_procs[3].m_priority = LOWEST;
+	g_test_procs[4].m_priority = MEDIUM;
+	g_test_procs[5].m_priority = LOW;
+	
+	null_proc.mpf_start_pc = &null_process;
+ 
+	//case one: 
+	g_test_procs[0].mpf_start_pc = &proc1;
+	g_test_procs[1].mpf_start_pc = &proc2;
+	
+	g_test_procs[2].mpf_start_pc = &proc3;
+	g_test_procs[3].mpf_start_pc = &proc4;
+	g_test_procs[4].mpf_start_pc = &proc5;
+	g_test_procs[5].mpf_start_pc = &proc6;
+}
 
-extern void k_rtx_init(void);
-#define rtx_init() _rtx_init((U32)k_rtx_init)
-extern void __SVC_0 _rtx_init(U32 p_func);
+/**
+ * @brief: a process that prints five uppercase letters
+ *         and then yields the cpu.
+ */
 
-extern int k_release_processor(void);
-#define release_processor() _release_processor((U32)k_release_processor)
-extern int __SVC_0 _release_processor(U32 p_func);
+void null_process() {
+	while(1){
+		release_processor();
+	}
+}
 
-extern void *k_request_memory_block(void);
-#define request_memory_block() _request_memory_block((U32)k_request_memory_block)
-extern void *_request_memory_block(U32 p_func) __SVC_0;
-/* __SVC_0 can also be put at the end of the function declaration */
+void proc1(void) {
+	void* mem_ptr[10];
+	int i;
+	while(1){
+		for(i = 0; i < 10; i ++) {
+			mem_ptr[i] = request_memory_block();
+			#ifdef DEBUG_0
+			printf("proc1 has %d memory blocks\n", i+1);
+			#endif /* DEBUG_0 */
+		}
+		release_processor(); // Proc2
+		for(i = 0; i < 10; i ++) {
+			if(mem_ptr[i] != NULL){
+				release_memory_block(mem_ptr[i]);
+			}
+		}
+		release_processor(); //Proc4
+		
+		for(i = 0; i < 10; i ++) {
+			mem_ptr[i] = request_memory_block();
+			#ifdef DEBUG_0
+			printf("proc1: test3_proc1 has %d memory blocks\n", i+1);
+			#endif /* DEBUG_0 */
+		}
+		
+		#ifdef DEBUG_0
+		printf("proc1: test4_This should not be printed as proc1 gets blocked.");
+		#endif /* DEBUG_0 */
+	}
+}
 
-extern int k_release_memory_block(void *);
-#define release_memory_block(p_mem_blk) _release_memory_block((U32)k_release_memory_block, p_mem_blk)
-extern int _release_memory_block(U32 p_func, void *p_mem_blk) __SVC_0;
+void proc2(void){
+	void* mem_ptr[2];
+	int i;
+	while(1){
+		for(i = 0; i < 2; i ++) {
+			mem_ptr[i] = request_memory_block();
+			#ifdef DEBUG_0
+				printf("proc2: test1_proc2 gets blocked\n"); //test one it should not print anything 
+			#endif /* DEBUG_0 */
+		}
+		release_processor();
+	}
+}
+void proc3(void){}
+void proc4(void){
+	void* mem_ptr[5];
+	int i;
+	while(1){
+		#ifdef DEBUG_0
+		printf("proc4: test6_this is the last print.\n");
+		#endif /* DEBUG_0 */
+		for(i = 0; i < 5; i++){
+			mem_ptr[i] = request_memory_block();
+			#ifdef DEBUG_0
+				printf("proc4: test2_proc4 gets %d memory blocks.\n", i+1); //prints 1 to 5
+			#endif /* DEBUG_0 */
+			
+		}
+		release_processor();
+		
+		for(i = 0; i < 5; i++){
+			if(mem_ptr[i] != NULL){
+				release_memory_block(mem_ptr[i]);
+			}
+		}
+		release_processor();
+		
+		#ifdef DEBUG_0
+		printf("proc4: test5_this gets printed.\n");
+		#endif /* DEBUG_0 */
+	}
+}
+void proc5(void){}
+void proc6(void){}
 
-#endif /* !RTX_H_ */
