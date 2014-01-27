05881399ca30ca114d1f906496832525f9e44a09
eva9202403 <yy2ho@uwaterloo.ca>
2014-01-26 21:23:01 -0500

    Update usr_proc.c

--- src/usr_proc.c
+++ src/usr_proc.c
@@ -19,39 +19,43 @@
 
 /* initialization table item */
 PROC_INIT g_test_procs[NUM_TEST_PROCS];
-/* initialization null process*/
-PROC_INIT null_proc;
+
+int test[6] = {0};
 
 void set_test_procs() {
 	int i;
-	for( i = 0; i < NUM_TEST_PROCS; i++ ) {
+
+	
+	g_test_procs[0].m_pid = (U32) 0;
+	g_test_procs[0].m_priority = LOWEST;
+	g_test_procs[0].m_stack_size = 0x100;
+	
+	for( i = 1; i < NUM_TEST_PROCS; i++ ) {
 		g_test_procs[i].m_pid = (U32)(i+1);
 		g_test_procs[i].m_stack_size = 0x100;
 	}
 	
-	null_proc.m_pid = (U32) 0;
-	null_proc.m_priority = LOWEST;
-	null_proc.m_stack_size = 0x100;
-	
-	g_test_procs[0].m_priority = HIGH;
 	g_test_procs[1].m_priority = HIGH;
-	g_test_procs[2].m_priority = MEDIUM;
-	g_test_procs[3].m_priority = LOWEST;
-	g_test_procs[4].m_priority = MEDIUM;
-	g_test_procs[5].m_priority = LOW;
+	g_test_procs[2].m_priority = HIGH;
+	g_test_procs[3].m_priority = MEDIUM;
+	g_test_procs[4].m_priority = LOWEST;
+	g_test_procs[5].m_priority = MEDIUM;
+	g_test_procs[6].m_priority = LOW;
 	
-	null_proc.mpf_start_pc = &null_process;
+	g_test_procs[0].mpf_start_pc = &null_process;
  
-	g_test_procs[0].mpf_start_pc = &proc1;
-	g_test_procs[1].mpf_start_pc = &proc2;
-	g_test_procs[2].mpf_start_pc = &proc3;
-	g_test_procs[3].mpf_start_pc = &proc4;
-	g_test_procs[4].mpf_start_pc = &proc5;
-	g_test_procs[5].mpf_start_pc = &proc6;
+	g_test_procs[1].mpf_start_pc = &proc1;
+	g_test_procs[2].mpf_start_pc = &proc2;
+	g_test_procs[3].mpf_start_pc = &proc3;
+	g_test_procs[4].mpf_start_pc = &proc4;
+	g_test_procs[5].mpf_start_pc = &proc5;
+	g_test_procs[6].mpf_start_pc = &proc6;
 }
 
 void null_process() {
 	while(1){
+		/*uart0_put_string("null process");
+		uart0_put_string("\n");*/
 		release_processor();
 	}
 }
@@ -65,18 +69,31 @@ void null_process() {
 void proc1(void) {
 	int i;
 	void* mem_ptr[10];
+	uart0_put_string("\n\r");
+	uart0_put_string("G006_test: START\n\r");
+	uart0_put_string("G006_test: total 6 tests\n\r");
 	while(1){
-		uart0_put_string("process 1 starts~\n");
+//		uart0_put_string("process 1 starts~\n");
 		//start requesting 10 memory blocks for proc1 
-		for(i = 0; i < 10; i ++) {
+		for(i = 0; i <= 9; i ++) {
 			mem_ptr[i] = request_memory_block();
-		  uart0_put_string("proc 1 requesting memory ");
-			uart0_put_char('0'+i);
-			uart0_put_string("\n");
+		//  uart0_put_string("proc 1 requesting memory ");
+		//	uart0_put_char('0'+i);
+		//	uart0_put_string("\n");
 		}
 		
-		//release processor to run the next process
+		if(sizeof(mem_ptr) == 10){
+			test[0] = 1;
+		}
 		release_processor();
+		//release processor to run the next process
+		if(test[0] == 2){
+			uart0_put_string("\n\r");
+			uart0_put_string("G006_test: test 1 OK\n\r");
+		}else{
+			uart0_put_string("\n\r");
+			uart0_put_string("G006_test: test 1 FAIL\n\r");
+		}
 		
 		//release all the memory blocks that proc1 has
 		for(i = 0; i < 10; i ++) {
@@ -101,11 +118,10 @@ void proc1(void) {
 void proc2(void){
 	void* mem_ptr;
 	while(1){
-		uart0_put_string("sidfjidfjiasjfoiajdfioasdf\n");
-		
+		test[0]++;
 		//request for one memory blcok
 		mem_ptr = request_memory_block();
-		
+		test[0]--;
 		//if proc2 gets blocked the strings below won't be printed
 		uart0_put_string("proc 2 requesting memory ");
 		uart0_put_string("\n");
@@ -155,15 +171,13 @@ void proc3(void){
  */
 void proc4(void){
 	int i = 0;
-  int ret_val;
+  void* ret_val;
   while (1) {
-		// a process that only prints five numbers 
-		if ( i != 0 && i%5 == 0 ) {
-			uart0_put_string("\n\r");
-			ret_val = release_processor();
-		}
-		uart0_put_char('0' + i%10);
-		i++;
+		ret_val = request_memory_block();
+		release_processor();
+		release_memory_block(ret_val);
+		release_processor();
+		
 	}
 }
 
