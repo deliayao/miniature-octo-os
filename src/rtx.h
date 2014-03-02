940c01227e046972b21a89901c08393ed53b537c
Eva Ho <yy2ho@uwaterloo.ca>
2014-03-01 19:37:58 -0500

    fixed the receive message type

--- src/rtx.h
+++ src/rtx.h
@@ -92,7 +92,7 @@ extern int _get_process_priority(U32 p_func, int process_id) __SVC_0;
 // IPC Management
 extern void *k_receive_message(int *sender_id);
 #define receive_message(sender_id) _receive_message((U32)k_receive_message, sender_id)
-extern int _receive_message(U32 p_func, int *sender_id) __SVC_0;
+extern void *_receive_message(U32 p_func, int *sender_id) __SVC_0;
 
 extern int k_delayed_send(int process_id, void *message_envelope, int delay);
 #define delayed_send(process_id, message_envelope, delay) _delayed_send((U32)k_delayed_send, process_id, message_envelope, delay)
