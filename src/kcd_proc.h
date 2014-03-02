3bc8a9c54f3649da5579bd8044f5a67ec518fa0e
Eva Ho <yy2ho@uwaterloo.ca>
2014-03-01 19:33:33 -0500

    add more functions

--- src/kcd_proc.h
+++ src/kcd_proc.h
@@ -1,40 +1,28 @@
-**
- * @file:   kcd_process.h
+/* @file:   kcd_proc.h
  * @brief:  Keyboard Command Decoder Process Management
  */
  
-#ifndef KCD_PROCESS_H_
-#define KCD_PROCESS_H_
-
-#include "rtx.h"
+#ifndef KCD_PROC_H_
+#define KCD_PROC_H_
 
 /* initialization table item */
-typedef struct register_table
-{
-   int register_pid;
-   char mtext;
-   struct register_table *next;
-}REGISTER_TABLE;
+typedef struct Command{
+   int command_pid;
+   char msgtext;
+   struct Command *Next;
+}Command;
 
-/* ----- Variables -----  
-extern unsigned int Image$$RW_IRAM1$$ZI$$Limit; 
+typedef struct KCDTable{
+	struct Command *head;
+	struct Command *last;
+}KCDTable;
 
-extern PROC_INIT g_proc_table[NUM_PROCS];
-
-extern PCB* processTable[NUM_PROCS]; // kernel process table
-extern PCB* currentProcess; // points to the current RUNNING process
-
-void memory_init(void);
-U32 *alloc_stack(U32 size_b);
-void *k_request_memory_block(void);
-int k_release_memory_block(void *);
-*/
-
-void kcdMain(void);
-void addItems(char mtext, int register_pid);
-void initTable();
-void resetUARTmsg();
+void KCDproc(void);
+void addCommand(char mtext, int register_pid);
+void checkMsgInfo(Letter* get_msginfo);
+void initializeKCDTable(void);
+int searchCommand(char mtext);
 
 
 
-#endif /* ! KCD_PROCESS_H_ */
+#endif /* ! KCD_PROC_H_ */
