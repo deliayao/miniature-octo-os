8c33ca2e999d35f2354fadca3f215b4614b47c68
Eva Ho <yy2ho@uwaterloo.ca>
2014-03-05 22:47:51 -0500

    change table type and add initializeKCDProcess()

--- src/kcd_proc.c
+++ src/kcd_proc.c
@@ -10,16 +10,43 @@
 #include "printf.h"
 #endif /* DEBUG_0 */
 
-// initialize the table, with linked lists you need to make sure the nodes are being created in valid memory
-struct KCDTable* table ;
-struct Command* search;
+/* --Definition-- */
+#define TABLE_SIZE 10
+
+PROC_INIT kcdProcess;
+
+// initialize the table with an array size of 10
+struct Command* table [TABLE_SIZE] = {0};
 char* buffer;
 
-void KCDproc(void) {
-		Letter* get_msginfo;
+//index used for searching & adding a command in to the kcd table 
+int index = 0;
+
+
+void initializeKCDProcess() {
+	kcdProcess.m_pid = (U32)KCD_PROCESS;
+	kcdProcess.m_priority = PRIVILEGED;
+	kcdProcess.m_stack_size = 0x100;
+	kcdProcess.mpf_start_pc = &kcd_proc;
+}
+
+void kcd_proc() {
+		int test;
+		Letter* get_msginfo = (Letter*)request_memory_block();
+	//buffer = (Letter*)request_memory_block();
+		while(1) {
+			get_msginfo = (Letter*) receive_message(&test);
+			uart0_put_string("RECIEVED SUCCESSFULLY\n\r");
+			release_processor();
+			release_memory_block((void *) get_msginfo);
+			//release_memory_block((void *) buffer);
+		}
+	/*	Letter* get_msginfo = (Letter*)request_memory_block();
 		int test;
 		int i = 0;
+		buffer->m_Type = DEFAULT;
 		initializeKCDTable();
+		
 
 	// void pointer pointing at the message body 
 		while(1) {
@@ -27,22 +54,22 @@ void KCDproc(void) {
 		get_msginfo = (Letter*) receive_message(&test);
 			
 		if(test == UART_IPROCESS) {
-			if(get_msginfo->m_Text[0] != '/n') {
-					buffer[i] = get_msginfo->m_Text[0];
+			if(get_msginfo->m_Text[0] != '\n') {
+					buffer->m_Text[i] = get_msginfo->m_Text[0];
 					i++;
 			}else{
-					i = 0;
-					checkMsgInfo(buffer);
+				i = 0;
+				checkMsgInfo(buffer);
 			}
-			// call CRT
-		}else {
+		// call CRT
+		} else {
 			checkMsgInfo(get_msginfo);
 		}
 		
 
 		//get_msginfo = (Letter *)NULL;
 		release_processor();
-	}
+	}*/
 }
 
 void checkMsgInfo(Letter* get_msginfo) {
@@ -54,7 +81,7 @@ void checkMsgInfo(Letter* get_msginfo) {
 						if(searchCommand(get_msginfo->m_Text[1]) != -1) { // when it found the m_Text[1] on the table
 								delayed_send(searchCommand(get_msginfo->m_Text[1]), (void*) get_msginfo, 100);
 						}else { // if it could not find m_Text[1] on the table then just print it for now
-							uart1_put_char(get_msginfo->m_Text[1]);
+									
 						}
 			}
 		} else if (get_msginfo->m_Type == KCD_REG) { // if it is the command string, then add it to the table
@@ -65,34 +92,22 @@ void checkMsgInfo(Letter* get_msginfo) {
 
 
 void addCommand(char mtext, int register_pid) {
-	struct Command* newCommand = table->last->Next;
+	struct Command* newCommand;
 	newCommand->command_pid = register_pid;
   newCommand->msgtext = mtext;
-	newCommand->Next = (Command *) NULL;
 	
-	if(table->head == NULL){
-		table->head = newCommand;
-		table->last = newCommand;
-	}else{
-		table->last->Next = newCommand;
-		table->last = newCommand;
+	for(index = 0; index < TABLE_SIZE; index++){
+		if(table[index] == (Command *) NULL){
+			table[index] = newCommand;
+		}
 	}
 }
 
-void initializeKCDTable(void) {
-    // make sure last node points to null
-		table = (KCDTable *)request_memory_block();
-    table->head = (Command *)NULL;
-		table->last = (Command *)NULL;
-}
-
 int searchCommand(char mtext){
-	search = table->head;
-	while(search != NULL){
-		if(search->msgtext == mtext){
-			return search->command_pid;
+	for(index = 0; index < TABLE_SIZE; index++){
+		if(table[index]->msgtext == mtext){
+			return table[index]->command_pid;
 		}
-		search = search->Next;		
 	}
 	// return -1 if it does not find the mtext on the table, else return the process id that register the command
 	return -1; 
